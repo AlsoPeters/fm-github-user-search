fef57957bf31db43071f35978fca872fcadf7448
Kyle <Kyle.fire@gmail.com>
2022-09-01 11:57:05 +0900

    added drop shadow to light mode styling

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -5,7 +5,7 @@ import Info from './Info';
 
 export default function Body() {
   return (
-    <div className='dark:bg-DM-dark-blue bg-LM-white rounded-xl my-4 px-6 py-4 flex flex-col gap-4'>
+    <div className='dark:bg-DM-dark-blue bg-LM-white rounded-xl my-4 px-6 py-4 flex flex-col gap-4 shadow-lg'>
       <Info />
       <Stats />
       <Social />
